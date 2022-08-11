fe62f0b70d6d19b47469e8fa327dc8a66bb6a466
aaishwaryakulkarni <38525159+aaishwaryakulkarni@users.noreply.github.com>
2022-08-11 16:06:00 +0530

    Add files via upload

--- linkedLists/rotate_list_k_times.py
+++ linkedLists/rotate_list_k_times.py
@@ -2,6 +2,29 @@
 61. Rotate List
 
 Given the head of a linked list, rotate the list to the right by k places.
+
+
+Approach:
+
+if k < length we can directly rotate it k times, but if k >= length, we need to avoid rotating unnecessary times
+
+if length of list is 5
+and k = 12, it means 5 + 5 + 2 = so we will twice rotate to the same original list and after that we will rotate 2 times.
+Therefore rotating 10 times and then 2 times is a waste!
+Therefore we will always do, k = k%length
+This will give us the reminder which is the number of times we need to rotate the original list.
+
+therefore, if k =12 and n = 5
+
+k = 12%5 = 2
+So we need to rotate the original list by k = 2 times.
+
+Before doing this we will create a circular linked list by attaching last element to head.
+
+Then we will go to the (length - k)th node. Make the  (length - k).next node head and (length - k).next = null
+This will break the cycle at the kth rotation.
+
+
 """
 
 class Node:
@@ -76,23 +99,30 @@ class MyLinkedList:
 
 			i = i + 1
 
-	def rotate_k_times(self, n):
+	def rotate_k_times(self, k):
 
-		slow = self.head
-		fast = slow
+		cur_node = self.head
 
-		for i in range(n):
-			fast = fast.next
+		#calculate the length
+		length = 1
+		while(cur_node.next != None):
+			cur_node = cur_node.next
+			length = length + 1
 
-		while(fast.next != None):
-			slow = slow.next
-			fast = fast.next
+		#make a circular linked list
+		cur_node.next = self.head
 
+		#Calculate new k
+		k = k%length
+		k = length - k
 
-		fast.next = self.head
-		self.head = slow.next
+		#go to the new kth position and dettach the remaining list and make it head.
+		for i in range(k):
+			cur_node = cur_node.next
 
-		slow.next = None
+		self.head = cur_node.next
+		cur_node.next = None
+
 
 
 # creating a linked list
@@ -109,5 +139,8 @@ myLinkedList.addAtTail(9)
 
 myLinkedList.printLinkedList()
 
-myLinkedList.rotate_k_times(6)
+myLinkedList.rotate_k_times(20)
 myLinkedList.printLinkedList()
+
+
+
