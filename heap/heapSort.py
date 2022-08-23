e4177ec12748987616d95802cb4457ab9ba3b6b7
aaishwaryakulkarni <38525159+aaishwaryakulkarni@users.noreply.github.com>
2022-08-23 21:28:50 +0530

    Add files via upload

---
+++ heap/heapSort.py
@@ -0,0 +1,21 @@
+"""
+Heap Sort
+"""
+
+
+def heapify(nums, n, i):
+
+	smallest = i
+	left = 2 * i + 1
+	right = 2 * i + 2
+
+	if((smallest < n) and (nums[]))
+
+
+def heapSort(nums):
+
+	for i in range(len(nums)//2-1,-1,-1)
+		heapify(nums, len(nums), i)
+
+nums = [10,30,50,20,35,15]
+print(heapSort(nums))
